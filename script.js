5a514136b6d54ac6acdf93c2199acc66242e1ee5
Boye <samuel.boye95@yahoo.com>
2022-06-03 18:43:08 +0100

    add working on responsive navbar javascript

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-// T
+// Navbar display script
 
+const body = document.querySelector("body");
+const ham = document.querySelector(".ham-burger");
+const nav = document.querySelector("#nav-res");
 
+(function () {
+  ham.addEventListener("click", () => {
+    if (nav.style.display === "flex") {
+      nav.style.display = "none";
+    } else {
+        nav.style.display = "flex";
+    }
+  });
+})();
