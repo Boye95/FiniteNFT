835282644d98330d015a1ae32a4edd3834456bfe
Boye <samuel.boye95@yahoo.com>
2022-06-04 19:22:09 +0100

    add wip

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ const ham = document.querySelector(".ham-burger");
 const nav = document.querySelector("#nav-res");
 const closeBtn = document.querySelector(".closeBtn");
 const overlay = document.querySelector(".overlay");
+let mQuery800 = window.matchMedia("(max-width: 800px)");
+let condition = 0;
 
 (function () {
-  let mQuery800 = window.matchMedia("(max-width: 800px)");
-
   ham.addEventListener("click", () => {
     if (mQuery800.matches) {
       nav.style.width = "100vw";
@@ -27,5 +27,18 @@ const overlay = document.querySelector(".overlay");
     nav.style.right = "-40vw";
     body.style.overflow = "auto";
     overlay.style.display = "none";
+  });
+
+  mQuery800.addEventListener("change", (mQuery800) => {
+    if (mQuery800.matches) {
+      nav.style.width = "100vw";
+      nav.style.right = "0";
+      body.style.overflow = "hidden";
+    } else {
+      nav.style.width = "40vw";
+      nav.style.right = "0";
+      body.style.overflow = "hidden";
+      overlay.style.display = "block";
+    }
   });
 })();
