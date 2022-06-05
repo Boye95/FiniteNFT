7c7f85b8f7081ba751bd479394bf09943231f59b
Boye <samuel.boye95@yahoo.com>
2022-06-05 22:32:30 +0100

    add side navbar working perfectly

--- script.js
+++ script.js
@@ -6,39 +6,91 @@ const nav = document.querySelector("#nav-res");
 const closeBtn = document.querySelector(".closeBtn");
 const overlay = document.querySelector(".overlay");
 let mQuery800 = window.matchMedia("(max-width: 800px)");
-let condition = 0;
 
-(function () {
-  ham.addEventListener("click", () => {
-    if (mQuery800.matches) {
-      nav.style.width = "100vw";
-      nav.style.right = "0";
-      body.style.overflow = "hidden";
-    } else {
-      nav.style.width = "40vw";
-      nav.style.right = "0";
-      body.style.overflow = "hidden";
-      overlay.style.display = "block";
-    }
-  });
 
-  closeBtn.addEventListener("click", () => {
-    nav.style.width = "0";
-    nav.style.right = "-40vw";
-    body.style.overflow = "auto";
-    overlay.style.display = "none";
-  });
+// This code was refactored by plamoni from Commit Your Code Discord server.
+let menuOpen = false;
 
-  mQuery800.addEventListener("change", (mQuery800) => {
-    if (mQuery800.matches) {
-      nav.style.width = "100vw";
-      nav.style.right = "0";
-      body.style.overflow = "hidden";
-    } else {
-      nav.style.width = "40vw";
-      nav.style.right = "0";
-      body.style.overflow = "hidden";
-      overlay.style.display = "block";
-    }
-  });
-})();
+function showMenu() {
+  menuOpen = true;
+  body.style.overflow = "hidden";
+  nav.style.right = "0";
+
+  if (mQuery800.matches) {
+    nav.style.width = "100vw";
+  } else {
+    nav.style.width = "40vw";
+    overlay.style.display = "block";
+  }
+}
+
+function hideMenu() {
+  menuOpen = false;
+
+  nav.style.width = "0";
+  nav.style.right = "-40vw";
+  body.style.overflow = "auto";
+  overlay.style.display = "none";
+}
+
+ham.addEventListener("click", showMenu);
+closeBtn.addEventListener("click", hideMenu);
+mQuery800.addEventListener("change", () => {
+  if (menuOpen) {
+    showMenu();
+  }
+});
+
+// Code for product offer countdown
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// (function () {
+//   ham.addEventListener("click", () => {
+//     if (mQuery800.matches) {
+//       nav.style.width = "100vw";
+//       nav.style.right = "0";
+//       body.style.overflow = "hidden";
+//     } else {
+//       nav.style.width = "40vw";
+//       nav.style.right = "0";
+//       body.style.overflow = "hidden";
+//       overlay.style.display = "block";
+//     }
+//   });
+
+//   closeBtn.addEventListener("click", () => {
+//     nav.style.width = "0";
+//     nav.style.right = "-40vw";
+//     body.style.overflow = "auto";
+//     overlay.style.display = "none";
+//   });
+
+//   mQuery800.addEventListener("change", (mQuery800) => {
+//     if (mQuery800.matches) {
+//       nav.style.width = "100vw";
+//       nav.style.right = "0";
+//       body.style.overflow = "hidden";
+//     } else {
+//       nav.style.width = "40vw";
+//       nav.style.right = "0";
+//       body.style.overflow = "hidden";
+//       overlay.style.display = "block";
+//     }
+//   });
+// })();
