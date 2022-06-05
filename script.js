f2b3ce66c820206b2b67d007ba9b125233e5f843
Boye <samuel.boye95@yahoo.com>
2022-06-05 23:02:37 +0100

    add basically done,

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ const closeBtn = document.querySelector(".closeBtn");
 const overlay = document.querySelector(".overlay");
 let mQuery800 = window.matchMedia("(max-width: 800px)");
 
-
-// This code was refactored by plamoni from Commit Your Code Discord server.
+// This code was written by me and refactored by plamoni from Commit Your Code Discord server.
 let menuOpen = false;
 
 function showMenu() {
@@ -41,24 +40,42 @@ mQuery800.addEventListener("change", () => {
   }
 });
 
-// Code for product offer countdown
+// Code for product promo countdown
+// The code below is gotten from w3schools.com. THis is the page link
+// https://www.w3schools.com/howto/howto_js_countdown.asp
 
+// It has been edited to accommodate the needs of this project.
 
+let beep = document.querySelector("#beep");
 
+// Set the date we're counting down to
+let countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
 
+// Update the count down every 1 second
+let x = setInterval(function () {
+  // Get today's date and time
+  let now = new Date().getTime();
 
+  // Find the distance between now and the count down date
+  let distance = countDownDate - now;
 
+  // Time calculations for days, hours, minutes and seconds
+  // let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+  // Output the result in an element with id="demo"
+  beep.innerHTML = hours + "h: " + minutes + "m: " + seconds + "s ";
 
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    beep.innerHTML = "EXPIRED";
+  }
+}, 1000);
 
-
-
-
-
-
-
-
-
+// the original code
 
 // (function () {
 //   ham.addEventListener("click", () => {
